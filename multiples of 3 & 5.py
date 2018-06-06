22f7c64ea56e918eda009f330bd284b8f4e8cf84
ROHAN TAKKE <37206921+takke2607@users.noreply.github.com>
2018-06-06 13:06:51 +0530

    Add files via upload
    simple program to find the multiples of 3 and 5 and do their summation..!!

---
+++ multiples of 3 & 5.py
@@ -0,0 +1,15 @@
+list3 = []
+list5 = []
+for i in range(1,1000):
+    if i % 3 == 0:
+        list3.append(i)
+    elif i % 5 == 0:
+        list5.append(i)
+    else:
+        continue
+
+final_answer = sum(list3) + sum(list5)
+print "final answer:%d"%(final_answer)
+print list3
+print list5
+print "hello"
